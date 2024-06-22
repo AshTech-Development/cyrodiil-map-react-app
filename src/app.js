ac9aa1b6eccaf057e5ba62bc685e66baecd9f85d
AshTech-Development <ashtechdevelopment0@gmail.com>
2024-06-22 09:37:26 -0400

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MapDataComponent from './MapDataComponent';
+
+function App() {
+  return (
+    <div>
+      <MapDataComponent />
+    </div>
+  );
+}
+
+export default App;
